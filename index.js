51925b0609f6386fbd11096bf54a3dd96ea8b89c
lesiamon <lesiamon.mpoe@student.moringaschool.com>
2023-10-05 17:44:25 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+// Declare companyName as let variable
+let companyName = 'Scuber' ;
+// Declare compaanyName as a const variable in a separate block
+{
+const companyName = 'Scuber' ;
+}
+
+// Declare mostProfitableNeighborhood as a let variable
+let mostProfitableNeighborhood = 'Chelsea' ;
+
+//Declare companyCeo as a const variable
+let companyCeo = 'Susan Smith' ;
